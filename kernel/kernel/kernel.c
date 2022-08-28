cd183854673208ddcc6eb57158c9ffde80e93dff
Christoph Lauper <christoph.lauper@digitecgalaxus.ch>
2022-08-28 19:36:28 +0200

    Add 🍖

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -4,5 +4,10 @@
 
 void kernel_main(void) {
 	terminal_initialize();
-	printf("Hello, kernel World!\n");
+	printf("Hello, kernel World!\nSo Long.\n");
+	printf("\t\t\ta\t\t\tb\t\t\tc\t\t\td\t\t\te\t\t\tf\t\t\tSee ya later.");
+	for (int i = 0; i < 100; i++) {
+		printf("%d", i);
+		printf("\n");
+	}
 }
