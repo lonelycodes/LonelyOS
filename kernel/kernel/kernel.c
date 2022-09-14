322bf8598fae349396037661f46c38d74d37774d
Christoph Lauper <christoph.lauper@digitecgalaxus.ch>
2022-09-14 18:52:51 +0200

    Better hello world

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -4,10 +4,5 @@
 
 void kernel_main(void) {
 	terminal_initialize();
-	printf("Hello, kernel World!\nSo Long.\n");
-	printf("\t\t\ta\t\t\tb\t\t\tc\t\t\td\t\t\te\t\t\tf\t\t\tSee ya later.");
-	for (int i = 0; i < 100; i++) {
-		printf("%d", i);
-		printf("\n");
-	}
+	printf("Hello, kernel World!\n\n\n\n\n\n\n\n\n\n\n\n\tso long\n");
 }
