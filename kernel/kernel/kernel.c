28372161e726c272ee418a71aae0cf7e7be02644
Christoph Lauper <christoph.lauper@digitecgalaxus.ch>
2022-09-14 21:03:00 +0200

    Test SSP

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
+#include <string.h>
 
 #include <kernel/tty.h>
+
+void test_ssp(const char* str)
+{
+	printf("hello!");
+	char buffer[16];
+	strcpy(buffer, str);
+	printf(str);
+	printf(buffer);
+}
 
 void kernel_main(void) {
 	terminal_initialize();
 	printf("LonelyOS v0\n\n\n\n\n\n\n\n\n\n\n\n\tso long");
+	test_ssp("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
 }
--- libc/include/sys/cdefs.h
+++ libc/include/sys/cdefs.h
@@ -1,5 +1,6 @@
 #ifndef _SYS_CDEFS_H
 #define _SYS_CDEFS_H 1
+#define NULL ((void*)0)
 
 #define __lonelyos_libc 1
 
