256cb421e202a73f399795bb654d38ee463e2c60
Christoph Lauper <christoph.lauper@digitecgalaxus.ch>
2022-09-14 21:02:37 +0200

    change cgk_guard to dummy AAAA

--- libc/stdlib/ssp.c
+++ libc/stdlib/ssp.c
@@ -1,18 +1,17 @@
-#include <stdint.h>
 #include <stdlib.h>
- 
+#include <stdio.h>
+
 #if UINT32_MAX == UINTPTR_MAX
-#define STACK_CHK_GUARD 0xe2dee396
+#define STACK_CHK_GUARD 0x10101010
 #else
-#define STACK_CHK_GUARD 0x595e9fbd94fda766
+#define STACK_CHK_GUARD 0x1010101010101010
 #endif
  
-uintptr_t __stack_chk_guard = STACK_CHK_GUARD;
+unsigned long __stack_chk_guard = STACK_CHK_GUARD;
  
 __attribute__((noreturn))
 void __stack_chk_fail(void)
 {
-
 #if defined(__is_libk)
 	// TODOC: Add proper kernel panic.
 	printf("kernel: panic: __stack_chk_fail()\n");
