0bb037373b795dfaa1f488d605898ed9ea014155
Christoph Lauper <christoph.lauper@digitecgalaxus.ch>
2022-09-26 13:48:12 +0200

    WIP GDT

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -2,9 +2,11 @@
 #include <string.h>
 
 #include <kernel/tty.h>
+#include "../include/kernel/gdt.h"
 
 void kernel_main(void) {
 	terminal_initialize();
 	printf("LonelyOS v0\n");
+	gdt_init();
 	printf("> ");
 }
